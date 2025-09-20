cb5ecdf4d26e76860ffef282fc395f3ade875af2
heavenlytalisman <173621005+heavenlytalisman@users.noreply.github.com>
2025-09-20 15:38:08 +0530

    Add: added styling

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,28 +1,48 @@
+import { useEffect } from 'react';
 import { auth, provider } from '../firebase-config.js';
-import { signInWithPopup } from 'firebase/auth';
-
+import { signInWithPopup, signInWithRedirect, getRedirectResult } from 'firebase/auth';
 import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 
 export const Auth = (props) => {
-    const { setIsAuth } = props
+  const { setIsAuth } = props;
 
-    const signInWithGoogle = async() => {
-        try {
-            const result = await signInWithPopup(auth, provider);
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  const signInWithGoogle = async () => {
+    try {
+      if (isMobile) {
+        await signInWithRedirect(auth, provider);
+      } else {
+        const result = await signInWithPopup(auth, provider);
+        const idToken = await result.user.getIdToken();
+        cookies.set("auth-token", idToken, { path: "/", secure: true, sameSite: "strict" });
+        setIsAuth(true);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (isMobile) {
+      getRedirectResult(auth)
+        .then(async (result) => {
+          if (result) {
             const idToken = await result.user.getIdToken();
-            cookies.set("auth-token", idToken, { path: "/" });
+            cookies.set("auth-token", idToken, { path: "/", secure: true, sameSite: "strict" });
             setIsAuth(true);
-        } catch(err) {
-            console.error(err);
-        }
-    };
+          }
+        })
+        .catch(console.error);
+    }
+  }, [isMobile, setIsAuth]);
 
-    return (
-        <div className="auth">
-            <p> Sign In with Google to continue. </p>
-            <button onClick={signInWithGoogle}>Sign In with Google</button>
-        </div>
-    );
-}
+  return (
+    <div className="auth">
+      <p>Sign In with Google to continue.</p>
+      <button onClick={signInWithGoogle}>Sign In with Google</button>
+    </div>
+  );
+};
